2f47c85662c3e31a65b3c40c133f43b676f2da3a
Arannamoy Mondal <71834368+Arannamoy-Mondal@users.noreply.github.com>
2026-04-12 22:20:01 +0600

    Create Test Servo Motor.ino

---
+++ Arduino/Test Servo Motor.ino
@@ -0,0 +1,20 @@
+#include <Servo.h>
+
+Servo myServo;  
+int servoPin = 9;
+
+void setup() {
+  myServo.attach(servoPin); 
+}
+
+void loop() {
+
+  for (int pos = 0; pos <= 180; pos += 1) { 
+    myServo.write(pos);              
+    delay(15);                       
+  }
+  for (int pos = 180; pos >= 0; pos -= 1) { 
+    myServo.write(pos);              
+    delay(15);                       
+  }
+}
